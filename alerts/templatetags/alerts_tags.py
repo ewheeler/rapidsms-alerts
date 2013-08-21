cc6f473c48456ff9bf94245c3bd52df954277904
ewheeler <evanmwheeler@gmail.com>
2013-08-21 16:09:53 -0400

    fix templatetags

--- alerts/templatetags/alerts_tags.py
+++ alerts/templatetags/alerts_tags.py
@@ -7,20 +7,19 @@ from alerts.models import Notification
 register = template.Library()
 import json
 
-@register.simple_tag
-def alerts(request):
+@register.inclusion_tag("alerts/partials/alerts.html", takes_context=True)
+def alerts(context):
+    request = context['request']
+    alerts = itertools.chain(*(g for g in get_alert_generators('alert', request=request, context=context) if g is not None))
+    return {"alerts": alerts}
 
-
-    alerts = itertools.chain(*(g for g in get_alert_generators('alert', request) if g is not None))
-    return render_to_string("alerts/partials/alerts.html",
-                            {"alerts": alerts})
-    
-@register.simple_tag
-def notifications(request):
+@register.simple_tag(takes_context=True)
+def notifications(context):
+    request = context['request']
     notifs = Notification.objects.filter(is_open=True, visible_to__user=request.user)
     data = json.dumps([notif.json(request.user) for notif in notifs])
 
     return render_to_string("alerts/partials/notifications.html",
                             {"notifs": notifs,
                              "notif_data": data}, context_instance=RequestContext(request))
-    
+

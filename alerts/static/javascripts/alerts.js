86f13fdeac0e9b80db1a77d4094ef3319893e60a
ewheeler <evanmwheeler@gmail.com>
2013-08-21 16:12:22 -0400

    include csrf token with ajax requests

--- alerts/static/javascripts/alerts.js
+++ alerts/static/javascripts/alerts.js
@@ -1,4 +1,34 @@
 
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+// TODO make csrf cookie name configurable!
+var csrftoken = getCookie('openmrs_csrftoken');
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    crossDomain: false, // obviates need for sameOrigin test
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type)) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
 
 function Alert (div, raw_data) {
   this.init = function() {
